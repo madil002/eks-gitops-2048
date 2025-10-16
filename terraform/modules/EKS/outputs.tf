f7391bd140640260bae9c2a49bedb2e683a4bdd3
madil002 <muabdul.adil@gmail.com>
2025-10-16 16:45:12 +0100

    add helm provider

--- terraform/modules/EKS/outputs.tf
+++ terraform/modules/EKS/outputs.tf
@@ -4,3 +4,7 @@ output "cluster_name" {
 output "cluster_sg_id" {
   value = aws_eks_cluster.main.vpc_config[0].cluster_security_group_id
 }
+
+output "cluster_endpoint" {
+  value = aws_eks_cluster.main.vpc_config[0].endpoint_public_access
+}
--- terraform/provider.tf
+++ terraform/provider.tf
@@ -5,5 +5,21 @@ terraform {
       source  = "hashicorp/aws"
       version = "6.16.0"
     }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "3.0.2"
+    }
+  }
+}
+
+provider "helm" {
+  kubernetes = {
+    host                   = module.EKS.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+    exec = {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      args        = ["eks", "get-token", "--cluster-name", module.EKS.cluster_name]
+      command     = "aws"
+    }
   }
 }
